246e249acd8bbf1d16085b9456b699ab53db77fd
Otst0y <114588112+Otst0y@users.noreply.github.com>
2023-04-28 15:51:21 +0300

    Add files via upload

---
+++ Java-Advanced_14/src/main/java/Card.java
@@ -0,0 +1,58 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Card {
+    private Integer id;
+    private Integer total;
+    private String name;
+    private Set<Item> items = new HashSet<Item>();
+
+    public Card(Integer total, String name) {
+        this.total = total;
+        this.name = name;
+    }
+
+    public Card() {}
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getTotal() {
+        return total;
+    }
+
+    public void setTotal(Integer total) {
+        this.total = total;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(Set<Item> items) {
+        this.items = items;
+    }
+
+    @Override
+    public String toString() {
+        return "Card{" +
+                "id=" + id +
+                ", total=" + total +
+                ", name='" + name + '\'' +
+                ", items=" + items +
+                '}';
+    }
+}
---
+++ Java-Advanced_14/src/main/java/Item.java
@@ -0,0 +1,47 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Item {
+    private Integer id;
+    private Integer total;
+    private Set<Card> carts = new HashSet<Card>();
+
+    public Item(Integer total) {
+        this.total = total;
+    }
+
+    public Item() {}
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getTotal() {
+        return total;
+    }
+
+    public void setTotal(Integer total) {
+        this.total = total;
+    }
+
+    public Set<Card> getCarts() {
+        return carts;
+    }
+
+    public void setCarts(Set<Card> carts) {
+        this.carts = carts;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "id=" + id +
+                ", total=" + total +
+                ", cards=" + carts +
+                '}';
+    }
+}
